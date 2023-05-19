83ed1f752e5c53a5baf6fd08d7e71154d532f884
Kareem Atef <kareematef0102@gmail.com>
2023-05-19 22:25:01 +0300

    initial commit

---
+++ project.m
@@ -0,0 +1,33 @@
+% Step 1: Preparing the environment
+% Install Computer Vision Toolbox if not already installed
+
+% Step 2: Loading the video
+videoFile = '.\videos\Adel Imam .mp4';
+videoReader = VideoReader(videoFile);
+frameRate = videoReader.FrameRate;
+frameWidth = videoReader.Width;
+frameHeight = videoReader.Height;
+
+% Step 3: Face detection
+faceDetector = vision.CascadeObjectDetector('FrontalFaceCART');
+
+while hasFrame(videoReader)
+    frame = readFrame(videoReader);
+    grayFrame = rgb2gray(frame);
+    
+    % Perform face detection
+    bboxes = step(faceDetector, grayFrame);
+    
+    % Draw rectangles around detected faces
+    detectedFrame = insertShape(frame, 'Rectangle', bboxes, 'LineWidth', 3);
+    
+    % Display the frame with detected faces
+    imshow(detectedFrame);
+    
+    % Pause for a short duration to display the frame
+    pause(1 / frameRate);
+end
+
+% Step 4: Cleanup
+release(videoReader);
+close all;
